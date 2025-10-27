dbd79e223dd6170d39cf53f5917939735927e91d
Arnoud <devoogdt.arnoud@gmail.com>
2025-10-27 16:41:11 +0100

    wtp and ota support

--- examples/bluecherry/main/main.cpp
+++ examples/bluecherry/main/main.cpp
@@ -20,16 +20,19 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
  */
 
-#include <stdio.h>
-#include <string.h>
+#include <esp_chip_info.h>
+#include <bsp/walter.hpp>
+#include <esp_system.h>
+#include "bluecherry.h"
+#include <sdkconfig.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
 #include <inttypes.h>
 #include <esp_log.h>
-#include <sdkconfig.h>
-#include <esp_chip_info.h>
-#include <esp_system.h>
-#include <esp_event.h>
-#include "bluecherry.h"
-#include <bsp/walter.hpp>
+#include <string.h>
+#include <stdio.h>
+
+#define BLUECHERRY_DEVICE_TYPE "walter01"
 
 /**
  * @brief The logging tag for this BlueCherry module.
@@ -61,6 +64,66 @@ static void bluecherry_msg_handler(uint8_t topic, uint16_t len, const uint8_t* d
   ESP_LOGI(TAG, "Received MQTT message of length %d on topic %02X: %.*s", len, topic, len, data);
 }
 
+esp_err_t nvs_write_str(const char* key, const char* value)
+{
+  nvs_handle_t handle;
+  ESP_ERROR_CHECK(nvs_open("bcztp_store", NVS_READWRITE, &handle));
+  ESP_ERROR_CHECK(nvs_set_str(handle, key, value));
+  ESP_ERROR_CHECK(nvs_commit(handle));
+  nvs_close(handle);
+  return ESP_OK;
+}
+
+esp_err_t nvs_read_str(const char* key, char* buf, size_t len)
+{
+  nvs_handle_t handle;
+  esp_err_t err = nvs_open("bcztp_store", NVS_READONLY, &handle);
+  if(err != ESP_OK)
+    return err;
+
+  err = nvs_get_str(handle, key, buf, &len);
+  nvs_close(handle);
+  return err;
+}
+
+static const char* bluecherry_ztp_bio_handler(bool read, bool secure, void* args)
+{
+  static char devcert[4096];
+  static char devkey[4096];
+
+  const char* keyname = secure ? "bcztp_key" : "bcztp_cert";
+
+  if(read) {
+    esp_err_t err =
+        nvs_read_str(keyname, secure ? devkey : devcert, secure ? sizeof(devkey) : sizeof(devcert));
+    if(err != ESP_OK) {
+      ESP_LOGW(TAG, "No %s found in NVS (err=0x%x)", keyname, err);
+      return NULL;
+    }
+    return secure ? devkey : devcert;
+  } else {
+    const char* data = (const char*) args;
+
+    nvs_handle_t handle;
+    ESP_ERROR_CHECK(nvs_open("bcztp_store", NVS_READWRITE, &handle));
+
+    if(data == NULL) {
+      ESP_LOGW(TAG, "Erasing %s from NVS", keyname);
+      nvs_erase_key(handle, keyname);
+      nvs_commit(handle);
+      nvs_close(handle);
+      return NULL;
+    }
+
+    ESP_ERROR_CHECK(nvs_set_str(handle, keyname, data));
+    ESP_ERROR_CHECK(nvs_commit(handle));
+    nvs_close(handle);
+
+    ESP_LOGI(TAG, "Stored %s in NVS", keyname);
+    return data;
+  }
+}
+
 /**
  * @brief The main application entrypoint.
  */
@@ -77,17 +140,25 @@ extern "C" void app_main(void)
     ESP_LOGE(TAG, "Could not start unified comms / connect a driver");
   }
 
-  vTaskDelay(pdMS_TO_TICKS(5000));
+  /* Initialize bluecherry with pre-provisioned keys */
+  // while (!bluecherry_init(devcert, devkey, bluecherry_msg_handler, NULL, true, 30)) {
+  //   ESP_LOGI(TAG, "Waiting for Initial bluecherry connection...");
+  //   vTaskDelay(pdMS_TO_TICKS(5000));
+  // }
 
-  while(true) {
-    if(bluecherry_init(devcert, devkey, bluecherry_msg_handler, NULL, true) == ESP_OK)
-      break;
-    vTaskDelay(pdMS_TO_TICKS(10000));
+  /* Initialize bluecherry with zero-touch provisioning */
+  while (bluecherry_init_ztp(bluecherry_ztp_bio_handler, NULL, BLUECHERRY_DEVICE_TYPE,
+                                      bluecherry_msg_handler, NULL, true, 30) != ESP_OK) {
+    ESP_LOGI(TAG, "Waiting for Initial bluecherry connection...");
+    vTaskDelay(pdMS_TO_TICKS(5000));
   }
 
   while(true) {
-    ESP_LOGI(TAG, "Publishing test message");
+    ESP_LOGI(TAG, "Publishing message");
     bluecherry_publish(0x84, strlen("Test message") + 1, (const uint8_t*) "Test message");
     vTaskDelay(pdMS_TO_TICKS(5000));
+    if(esp_task_wdt_status(NULL) == ESP_OK) {
+      esp_task_wdt_reset();
+    }
   }
 }
